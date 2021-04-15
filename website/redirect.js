636594ea71e85a1df36c4b43e58591a471de0586
Tijn <ps218465@edu.summacollege.nl>
2021-04-15 10:00:03 +0200

    website is fully responsive

--- website/redirect.js
+++ website/redirect.js
@@ -2,7 +2,7 @@ function redirect() {
     for (var i = 1; i < 12; i++) {
         var checkbox = document.getElementById("checkbox" + i);
         if (checkbox.checked == true) {
-            location.replace("/website/index.php");
+            location.replace("/project 2/Tijn-en-inanc/website/index.php");
             return;
         }
     }
